4de2ba6a23de908b9e596ede363d48ddbb332d69
Marx Roberto <marxmoita@gmail.com>
2020-02-09 10:39:46 -0300

    criando middleware de autenticação e o update do destinatário

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -13,8 +13,34 @@ class RecipientController {
     return res.json(users);
   }
 
+  async show(req, res) {
+    const user = await Recipient.findByPk(req.params.id);
+
+    return res.json(user);
+  }
+
   async update(req, res) {
-    return res.json({ ok: true });
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    } = await recipient.update(req.body, { where: req.params.id });
+
+    return res.json({
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,24 @@
 import { Router } from 'express';
-import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+
+routes.use(authMiddleware);
+
 routes.get('/users', UserController.index);
 
 routes.post('/sessions', SessionController.store);
 
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
-routes.put('/recipients', RecipientController.update);
+routes.get('/recipients/:id', RecipientController.show);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
