4321e9a9fedacc5273df7d76980fabaa4a881158
Marx Roberto <marxmoita@gmail.com>
2020-02-10 10:39:44 -0300

    Adicionando validação na entrada de dados com Yup

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.string()
+        .required()
+        .length(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const recipient = await Recipient.create(req.body);
 
     return res.json(recipient);
@@ -20,6 +37,20 @@ class RecipientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.string().length(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const recipient = await Recipient.findByPk(req.params.id);
 
     const {
